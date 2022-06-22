a5bee0d00688a365f364578e571dc5fb6219d14f
jerryntom <jaroslawtomczuk.ofi@gmail.com>
2022-06-22 16:58:33 +0200

    Responsive site patch - vipla internet
    * header hides on various header heights
    * new css - mobile.css (made for mobile view)
    * new mobile view

--- hideHeaderOnScroll.js
+++ hideHeaderOnScroll.js
@@ -1,14 +1,19 @@
 var prevScrollpos = window.pageYOffset;
+var headerHeight = "-120px";
+
+if (screen.width <= "480") {
+	headerHeight = "-210px";
+}
 
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } 
-  else {
-    document.getElementById("header").style.top = "-120px";
-  }
+	var currentScrollPos = window.pageYOffset;
+	
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById("header-div").style.top = "0";
+	} 
+	else {
+		document.getElementById("header-div").style.top = headerHeight;
+	}
 
-  prevScrollpos = currentScrollPos;
+	prevScrollpos = currentScrollPos;
 }
